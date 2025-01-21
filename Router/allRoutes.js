6a332a52e05da2186d63322de36e8e7a09dd7f0f
eurajane <jbouchend@gmail.com>
2025-01-21 21:44:45 +0100

    ajout footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,10 @@ export const allRoutes = [
     new Route("/covoit", "Accès aux covoiturages", "/pages/covoit.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/login", "Connexion", "/pages/login.html"),
+    new Route("/legalInfos", "Mentions légales", "/pages/legalInfos.html"),
+    new Route("/privacyPolicy", "Politique de confidentialité", "/pages/privacyPolicy.html"),
+    new Route("/cookieManagement", "Gestion des cookies", "/pages/cookieManagement.html"),
+    new Route("/cgu", "CGU", "/pages/cgu.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
