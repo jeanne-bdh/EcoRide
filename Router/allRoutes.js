8799547e12352ce5b1618947340cbbcd2db694f3
eurajane <jbouchend@gmail.com>
2025-01-21 16:40:53 +0100

    ajout navbar desktop

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,11 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/covoit", "Accès aux covoiturages", "/pages/covoit.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/login", "Connexion", "/pages/login.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
