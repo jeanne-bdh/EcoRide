d7db3ad96c85523ecf21a083576c6c1ee191aadd
eurajane <jbouchend@gmail.com>
2025-01-23 10:49:48 +0100

    ajout page login

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,10 +6,10 @@ export const allRoutes = [
     new Route("/covoit", "Accès aux covoiturages", "/pages/covoit.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/login", "Connexion", "/pages/login.html"),
-    new Route("/legalInfos", "Mentions légales", "/pages/legalInfos.html"),
-    new Route("/privacyPolicy", "Politique de confidentialité", "/pages/privacyPolicy.html"),
-    new Route("/cookieManagement", "Gestion des cookies", "/pages/cookieManagement.html"),
-    new Route("/cgu", "CGU", "/pages/cgu.html"),
+    new Route("/legalInfos", "Mentions légales", "/pages/mentions-legales/legalInfos.html"),
+    new Route("/privacyPolicy", "Politique de confidentialité", "/pages/mentions-legales/privacyPolicy.html"),
+    new Route("/cookieManagement", "Gestion des cookies", "/pages/mentions-legales/cookieManagement.html"),
+    new Route("/cgu", "CGU", "/pages/mentions-legales/cgu.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
