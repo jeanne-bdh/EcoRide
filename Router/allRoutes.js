7ca234b229e01abda6514cfb29d9ecd18bdcd49c
eurajane <jbouchend@gmail.com>
2025-01-27 23:05:41 +0100

    ajout login.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoit", "Accès aux covoiturages", "/pages/covoit.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/login", "Connexion", "/pages/login.html"),
+    new Route("/login", "Connexion", "/pages/login.html", "/pages/login.js"),
     new Route("/register", "Inscription", "/pages/register.html"),
     new Route("/legalInfos", "Mentions légales", "/pages/mentions-legales/legalInfos.html"),
     new Route("/privacyPolicy", "Politique de confidentialité", "/pages/mentions-legales/privacyPolicy.html"),
