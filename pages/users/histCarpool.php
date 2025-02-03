b30a6fbea2b8f68ff723552c2539a1bce85a11c6
eurajane <jbouchend@gmail.com>
2025-02-03 23:32:27 +0100

    ajout card carpool

--- pages/users/histCarpool.php
+++ pages/users/histCarpool.php
@@ -12,6 +12,15 @@ require_once __DIR__ . "/../templates/header.php";
     heroSection("Historique des covoiturages");
     ?>
 
+<div class="menu-session">
+        <a href="/pages/users/oldCarpool.php" class="card-session">
+            <h3>Covoiturages passés</h3>
+        </a>
+        <a href="/pages/users/futureCarpool.php" class="card-session">
+            <h3>Covoiturages à venir</h3>
+        </a>
+    </div>
+
 </section>
 
 <?php require_once __DIR__ . "/../templates/footer.php" ?>
