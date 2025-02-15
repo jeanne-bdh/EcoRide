251ea7dd0e04ee77c68f3c0429dc9fdef87e1697
eurajane <jbouchend@gmail.com>
2025-02-15 20:52:30 +0100

    add access carpool

--- pages/carpools/carpools_search.php
+++ pages/carpools/carpools_search.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . "/../templates/header.php";
-require_once __DIR__ . "/../libs/pdo.php";
-require_once __DIR__ . "/../libs/carpool.php";
+require_once __DIR__ . "/../../templates/header.php";
+require_once __DIR__ . "/../../libs/pdo.php";
+require_once __DIR__ . "/../../libs/carpool.php";
 
 ?>
 
@@ -10,15 +10,31 @@ require_once __DIR__ . "/../libs/carpool.php";
 
     <!-- HERO SECTION -->
     <?php
-    include_once __DIR__ . "/../templates/hero_section.php";
+    include_once __DIR__ . "/../../templates/hero_section.php";
     heroSection("Accès aux covoiturages");
+    ?>
 
-    $carpoolsSearch = getCarpoolBySearch($pdo);
+    <!-- FILTERS -->
+    <div class="form-filters">
+        <select class="form-select" id="floatingSelect" aria-label="Sélectionner des filtres">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <label for="floatingSelect">Works with selects</label>
+    </div>
+    </div>
 
-    foreach ($carpoolsSearch as $key => $carpoolSearch) {
-        require __DIR__ . '/../templates/carpool_card_visitor.php';
-    } ?>
+    <div class="container-carpool-visitor">
+        <?php
+        $carpoolsSearch = getCarpoolBySearch($pdo);
+
+        foreach ($carpoolsSearch as $key => $carpoolSearch) {
+            require __DIR__ . '/../../templates/carpool_card_visitor.php';
+        } ?>
+    </div>
 
 </section>
 
-<?php require_once __DIR__ . "/../templates/footer.php" ?>
+<?php require_once __DIR__ . "/../../templates/footer.php" ?>
