095aaf10e9ca5483ab47343d815c8b2ccf8baa31
eurajane <jbouchend@gmail.com>
2025-02-18 12:12:33 +0100

    update carpool-card-visitors responsive

--- templates/carpool_card_visitor.php
+++ templates/carpool_card_visitor.php
@@ -34,18 +34,18 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#757575" viewBox="0 0 16 16">
                     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                 </svg>
-                <p class="p-notes">??</p>
+                <p class="p-notes">4,5</p>
             </div>
-        </div>
-        <div class="green-tag">
+            <div class="green-tag">
                 <p><?= $carpoolSearch['type_voyage']; ?></p>
             </div>
+        </div>
         <div class="details-link">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#528DB9" class="chevron-double-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708" />
                 <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708" />
             </svg>
-            <a href="/pages/carpool_details.php?id_covoit=<?=$key ?>;">
+            <a href="/pages/carpool_details.php?id_covoit=<?= $key ?>;">
                 <p>Voir détails</p>
             </a>
         </div>
