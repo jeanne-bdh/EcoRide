775e9812811bd656be840fa55e811783d500048c
eurajane <jbouchend@gmail.com>
2025-05-14 16:01:39 +0200

    add view future carpools

--- libs/carpool.php
+++ libs/carpool.php
@@ -23,15 +23,16 @@ function getPastCarpoolByUser(PDO $pdo, int $userId): array
 function getFutureCarpoolByUser(PDO $pdo, int $userId): array
 {
     $stmt = $pdo->prepare(
-        'SELECT carpools.*, status_carpool.label_status_carpool, travel_types.label_travel_type
+        'SELECT carpools.*, status_carpool.label_status_carpool, travel_types.label_travel_type, users.pseudo AS driver_pseudo, carpools_users.id_users AS id_participant
         FROM carpools
-        JOIN status_carpool ON status_carpool.id_status_carpool = carpools.id_status_carpool
-        JOIN users ON users.id_users = carpools.id_users
-        JOIN cars ON cars.id_users = users.id_users
-        JOIN travel_types ON travel_types.id_travel_type = cars.id_travel_type
-        WHERE carpools.id_users = :id_users
-        AND date_depart > NOW()
-        ORDER BY date_depart ASC'
+        JOIN carpools_users ON carpools_users.id_carpool = carpools.id_carpool
+        JOIN status_carpool ON carpools.id_status_carpool = status_carpool.id_status_carpool
+        JOIN users ON carpools.id_users = users.id_users
+        JOIN cars ON cars.id_users = carpools.id_users
+        JOIN travel_types ON cars.id_travel_type = travel_types.id_travel_type
+        WHERE carpools.date_depart > CURRENT_DATE
+        AND carpools_users.id_users = :id_users
+        ORDER BY carpools.date_depart ASC'
     );
     $stmt->bindValue(':id_users', $userId, PDO::PARAM_INT);
     $stmt->execute();
--- pages/users/future-carpool/future_carpool.php
+++ pages/users/future-carpool/future_carpool.php
@@ -3,6 +3,7 @@
 require_once dirname(__DIR__, 3) . "/templates/header.php";
 require_once dirname(__DIR__, 3) . "/libs/pdo.php";
 require_once dirname(__DIR__, 3) . "/libs/carpool.php";
+require_once dirname(__DIR__, 3) . "/libs/review.php";
 
 ?>
 
@@ -20,6 +21,7 @@ require_once dirname(__DIR__, 3) . "/libs/carpool.php";
             $futureCarpool = false;
             foreach ($carpools as $carpool) {
                 $futureCarpool = true;
+                $averageNotes = getAverageNotes($pdo, $carpool['id_users']);
 
                 require dirname(__DIR__, 3) . "/templates/carpool_card.php";
             }
