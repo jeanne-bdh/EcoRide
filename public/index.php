08ae1a9396bd497d1326f8f677c9cff0d5705195
eurajane <jbouchend@gmail.com>
2025-09-08 08:59:44 +0200

    config index to deploy

--- public/index.php
+++ public/index.php
@@ -2,16 +2,26 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-use Dotenv\Dotenv;
 use App\Routing\Router;
 
 define('APP_ROOT', dirname(__DIR__));
-define('APP_ENV', $_ENV['DB_NAME']);
 
+// SESSION
+session_save_path('/home/bouchendhomme/tmp');
 session_start();
 
-$dotenv = Dotenv::createImmutable(__DIR__ . "/..", APP_ENV);
-$dotenv->load();
+// Récupération des variables d'environnement Alwaysdata
+$dbName = getenv('DB_NAME');
+$dbHost = getenv('DB_HOST');
+$dbPort = getenv('DB_PORT');
+$dbUser = getenv('DB_USER');
+$dbPassword = getenv('DB_PASSWORD');
+$mongoUri = getenv('MONGODB_URI');
+$pepper = getenv('PEPPER');
 
+// Tu peux définir APP_ENV si besoin
+define('APP_ENV', getenv('APP_ENV') ?: 'prod');
+
+// Instanciation du router
 $router = new Router();
 $router->handleRequest($_SERVER["REQUEST_URI"]);
